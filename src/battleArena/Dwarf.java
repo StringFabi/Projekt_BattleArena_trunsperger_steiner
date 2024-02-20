1f036bf40149c3869b480037892c71bb9261f05d
StringFabi <fabian.trunsperger@hak-reutte.ac.at>
2024-02-20 12:55:39 +0100

    added Constructor and Getter Setter and toString

--- src/battleArena/Dwarf.java
+++ src/battleArena/Dwarf.java
@@ -1,5 +1,47 @@
 package battleArena;
 
-public class Dwarf {
+public class Dwarf extends Characters{
+		private int calculateAttackPower;
+		private int dwarfCoconut;
+		
+		
+		public Dwarf(String name, int healthPower, boolean specialAbilityActive, int damage, int calculateAttackPower, int dwarfCoconut) {
+			super(name, healthPower, specialAbilityActive, damage);
+			this.calculateAttackPower = calculateAttackPower;
+			this.dwarfCoconut = dwarfCoconut;
+		}
 
+
+		public int getCalculateAttackPower() {
+			return calculateAttackPower;
+		}
+
+
+		public void setCalculateAttackPower(int calculateAttackPower) {
+			this.calculateAttackPower = calculateAttackPower;
+		}
+
+
+		public int getDwarfCoconut() {
+			return dwarfCoconut;
+		}
+
+
+		public void setDwarfCoconut(int dwarfCoconut) {
+			this.dwarfCoconut = dwarfCoconut;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Dwarf [calculateAttackPower=" + calculateAttackPower + ", dwarfCoconut=" + dwarfCoconut
+					+ ", getCalculateAttackPower()=" + getCalculateAttackPower() + ", getDwarfCoconut()="
+					+ getDwarfCoconut() + ", getName()=" + getName() + ", getHealthPower()=" + getHealthPower()
+					+ ", isSpecialAbilityActive()=" + isSpecialAbilityActive() + ", getDamage()=" + getDamage()
+					+ ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+					+ "]";
+		}
+		
+		
+		
 }
