eb14c2d071880abf679f5737115101e59d8f6ffe
StringFabi <fabian.trunsperger@hak-reutte.ac.at>
2024-03-05 12:08:34 +0100

    Added Constructor to Class BattleArena

--- src/battleArena/BattleArena.java
+++ src/battleArena/BattleArena.java
@@ -1,5 +1,17 @@
 package battleArena;
 
 public class BattleArena {
-		
+	int [] character = new int [2];
+	int [] character1;
+	int [] character2;
+	
+	
+	public BattleArena(int[] character, int[] character1, int[] character2) {
+		super();
+		this.character = character;
+		this.character1 = character1;
+		this.character2 = character2;
+	}
+	
+	
 }
