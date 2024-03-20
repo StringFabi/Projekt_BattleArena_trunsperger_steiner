49900581bad2b80e3ef52f3ddba0b365ca1e0fe5
IntFelix2024 <felix.steiner@hak-reutte.ac.at>
2024-03-20 19:22:06 +0100

    Test Changes

--- src/battleArena/BattleArena.java
+++ src/battleArena/BattleArena.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class BattleArena {
 
-
 	private CharacterRole character1;
 	private CharacterRole character2;
 	private CharacterRole winner;
